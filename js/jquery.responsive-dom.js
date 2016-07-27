868f4f6eb4441c3ee5a2961a46cbfb97d1e61c4e
Josh Parrett <jtparrett@me.com>
2016-07-27 17:39:53 +0100

    Object based re-write

--- js/jquery.responsive-dom.js
+++ js/jquery.responsive-dom.js
@@ -1,117 +1,59 @@
-/**
- * A jQuery plugin to move elements in the DOM based on media queries
- * @param options
- */
-(function(factory) {
-	'use strict';
+(function(window, $) {
+  var instances = [];
 
-	if (typeof define === 'function' && define.amd) {
-		define(['jquery'], factory);
-	} else {
-		factory(jQuery);
-	}
-}(function($) {
-	'use strict';
+  $.fn.responsiveDom = function(options){
+    var settings = $.extend({
+          appendTo: 'body',
+          mediaQuery: '(min-width: 0)',
+          callback: null
+        }, options);
 
-	$.fn.responsiveDom = function (options) {
-		// The settings object provides default settings.
-		// The options argument can override them.
-		var settings = $.extend({
-			appendTo: 'body',             // The provided object will be moved here...
-			mediaQuery: '(min-width: 0)', // ...when this media query is true.
-			callback: null                // If provided, the callback will run after DOM updates.
-		}, options);
+    return this.each(function(){
+      instances.push(new ResponsiveDOM($(this), settings));
+    });
+  };
 
-		var sourceEl = this;
-		var placeholder = null;
-		var isMoved = false;
+  $(window).on('resize', function(){
+    instances.forEach(function(inst){
+      inst.update();
+    });
+  });
 
-		/**
-		 * Initializes the plugin
-		 */
-		var init = function() {
-			// Update the DOM now...
-			updateDom();
+})(window, jQuery);
 
-			// ...and again when the window resizes
-			$(window).on('resize.responsiveDom', debounce(updateDom, 100));
-		};
+var ResponsiveDOM = function(el, opts){
+  this.el = el;
+  this.location = $(opts.appendTo);
+  this.opts = opts;
+  this.isMoved = false;
 
-		/**
-		 * Moves or reverts element DOM position if the media query conditions are met
-		 */
-		var updateDom = function() {
-			// Check if media query conditions are met
-			if (!isMoved && matchMedia(settings.mediaQuery).matches) {
-				moveElement();
-				isMoved = true;
-			} else if (isMoved && !matchMedia(settings.mediaQuery).matches) {
-				revertElement();
-				isMoved = false;
-			} else {
-				return;
-			}
+  this.makePlaceholder();
+  this.update();
+};
 
-			// Run callback function if provided
-			if (typeof settings.callback === 'function') {
-				settings.callback(isMoved);
-			}
-		};
+ResponsiveDOM.prototype.makePlaceholder = function(){
+  this.placeholder = $('<div />').insertAfter(this.el).hide();
+};
 
-		/**
-		 * Creates a placeholder at the element's current DOM position and moves the
-		 * element to its new position
-		 */
-		var moveElement = function() {
-			// Verify the source element still exists in the DOM
-			if (!document.contains || document.contains(sourceEl[0])) {
-				// Create placeholder so we can move it back if needed
-				placeholder = $('<span class="js-responsive-dom-placeholder"/>');
-				sourceEl.after(placeholder);
+ResponsiveDOM.prototype.update = function(){
+  var doesMatch = window.matchMedia(this.opts.mediaQuery).matches;
 
-				// Move element
-				$(settings.appendTo).eq(0).append(sourceEl);
-			}
-		};
+  if(!this.isMoved && doesMatch){
+    this.place();
+  } else if(this.isMoved && !doesMatch){
+    this.revert();
+  }
+};
 
-		/**
-		 * Returns element to its previous position in the DOM and removes the
-		 * placeholder element
-		 */
-		var revertElement = function() {
-			// Verify the placeholder still exists in the DOM
-			if (placeholder !== null && (!document.contains || document.contains(placeholder[0]))) {
-				// Move element back and remove placeholder
-				placeholder.after(sourceEl);
+ResponsiveDOM.prototype.place = function(){
+  this.el.appendTo(this.location);
+  if(this.opts.callback){
+    this.opts.callback(this);
+  }
+  this.isMoved = true;
+};
 
-				placeholder.remove();
-				placeholder = null;
-			}
-		};
-
-		/**
-		 * Returns a function that cannot be called in succession unless a specified
-		 * amount of time has passed
-		 * @param func - The function to debounce
-		 * @param wait - The wait time (ms) before running the function again
-		 * @returns The debounced function
-		 */
-		var debounce = function(func, wait) {
-			var timeout;
-
-			return function() {
-				clearTimeout(timeout);
-
-				timeout = setTimeout(function() {
-					func();
-				}, wait);
-			};
-		};
-
-		// Let's go!
-		init();
-
-		// Always return the target object to allow chaining.
-		return this;
-	};
-}));
+ResponsiveDOM.prototype.revert = function(){
+  this.el.insertBefore(this.placeholder);
+  this.isMoved = false;
+};
